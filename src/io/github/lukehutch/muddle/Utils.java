4c19789ccefbbb1de274c8884420f94972091108
Luke Hutchison <luke.hutch@gmail.com>
2015-09-28 13:51:56 -0700

    Update Utils.java

--- src/io/github/lukehutch/muddle/Utils.java
+++ src/io/github/lukehutch/muddle/Utils.java
@@ -68,6 +68,8 @@ public class Utils {
     public static void main(String[] args) {
         // float[] data = loadData("filename");
 
+        // PPG data available at: http://www.signalprocessingsociety.org/spcup2015/index.html
+        
         // Rows 1 and 2 are PPG signals
         float[] data = loadMat("/home/luke/Downloads/Training_data/DATA_01_TYPE01.mat", 1);
         // float[] hist = MUDDLE.generateAlternatingExtremumTypeFractionHistogram(data, 300);
